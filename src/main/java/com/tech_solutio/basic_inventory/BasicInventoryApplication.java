aed08f79cf451ee2140d606368ec5d566c68efc9
Victor Rivas <vrivas@parameta.co>
2025-10-23 22:12:08 -0300

    Initial commit

---
+++ src/main/java/com/tech_solutio/basic_inventory/BasicInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.tech_solutio.basic_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicInventoryApplication.class, args);
+	}
+
+}
